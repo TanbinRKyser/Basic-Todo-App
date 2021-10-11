b3f5a69004bf80db997fc4886f8c7f803616b057
Tanbin Rahid Kyser Mitul <6168653+TanbinRKyser@users.noreply.github.com>
2021-10-11 13:18:03 +0600

    Critical Vulnerability.
    Dummy Error as noob git user back then.

--- restful-web-services/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO todo(id,username,description,target_date,is_done)
-VALUES (10001,'tusker', 'Learn JPA', sysdate(), false);
-
-INSERT INTO todo(id,username,description,target_date,is_done)
-VALUES (10002, 'tusker', 'Learn Angular', sysdate(), false);
-
-INSERT INTO todo(id,username,description,target_date,is_done)
-VALUES (10003, 'tusker', 'Learn Python', sysdate(), false);
